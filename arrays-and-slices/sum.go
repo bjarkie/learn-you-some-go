5b59ad1144390fe775f68d5fa6fa2883b4eed637
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-15 11:53:51 +0000

    Refactor Sum to use the range keyword

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -2,8 +2,9 @@ package sum
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
+		// fmt.Println("Loop through array", number)
 	}
 	return sum
 }
