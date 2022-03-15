4eea40b32a40ca204515a88f82b09ae01bdf3760
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-15 13:18:19 +0000

    Create SumAll function returning sums for a list of slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,24 @@
 package sum
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lenghtOfNumbers := len(numbersToSum)
+	sums := make([]int, lenghtOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+		fmt.Println("Sums", numbers)
+	}
+
+	fmt.Println("Total", sums)
+	return sums
 }
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -2,6 +2,8 @@ package sum
 
 import "testing"
 
+import "reflect"
+
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -14,4 +16,14 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 1, 2, 9})
+	want := []int{3, 12}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
