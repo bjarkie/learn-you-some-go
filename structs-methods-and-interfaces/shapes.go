0acf4c2e28c17e854d18a211faa07ce3fe2340b0
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-15 15:01:19 +0000

    Add Triangle shape support and introduce table driven tests

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -6,6 +6,11 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -17,6 +22,10 @@ type Circle struct {
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func (c Circle) Area() float64 {
