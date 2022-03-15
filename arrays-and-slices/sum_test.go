9f651983255063a354e99d7e1e8ebbc12a52be68
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-15 13:55:26 +0000

    Refactor SumAll to SumAllTails to better understand slices

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,8 +1,9 @@
 package sum
 
-import "testing"
-
-import "reflect"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -22,6 +23,15 @@ func TestSumAll(t *testing.T) {
 
 	got := SumAll([]int{1, 2}, []int{0, 1, 2, 9})
 	want := []int{3, 12}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2, 5}, []int{0, 4, 5, 9})
+	want := []int{7, 18}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
