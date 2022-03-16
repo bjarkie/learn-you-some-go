577e67dd3fa757eb48253b966742883400ce47df
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-16 15:33:16 +0000

    Use select/case and goroutines to improve the performance and readability of our code

--- select/racer.go
+++ select/racer.go
@@ -1,26 +1,23 @@
 package racer
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-
-	fmt.Println("A: ", aDuration)
-	fmt.Println("B: ", bDuration)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
