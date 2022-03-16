059bc43f2a09864ec2ce53a628a492b3510978f3
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-16 14:21:57 +0000

    Use configurable sleeper in app

--- mocking/main.go
+++ mocking/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
