ba3c3c077ca4e1ecc89e76301b1c5309b2ac3e86
Bjarki Eliasen <bjarki.eliasen@gmail.com>
2022-03-16 10:27:57 +0000

    Return an error if specified search term was not found in the dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,14 +1,34 @@
 package dictionary
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, err := dictionary.Search("test")
+		fmt.Println("Value I got:", got)
+		fmt.Println("Error I got:", err)
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		value, err := dictionary.Search("unknown")
+		fmt.Println("Value I got:", value)
+		fmt.Println("Error I got:", err)
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
